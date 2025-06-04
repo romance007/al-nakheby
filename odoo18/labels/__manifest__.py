27c5630af29ae86d015cfa80971cd809321551a6
Mohammed <moh_romance@hotmail.com>
2025-06-04 14:47:03 +0300

    Rename module to Transfer Label Generator

--- odoo18/labels/__manifest__.py
+++ odoo18/labels/__manifest__.py
@@ -1,1 +1,18 @@
+{
+    'name': 'Transfer Label Generator',
+    'version': '1.0',
+    'summary': 'Custom label printing for transfer operations',
+    'description': 'Generates barcode labels for products during transfer, only if checkbox is enabled on product form.',
+    'category': 'Inventory',
+    'author': 'Mohammed Al-Nakheby',
+    'website': 'https://github.com/romance007',
+    'depends': ['stock'],
+    'data': [
+        'reports/report_reception_report_label.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
  
